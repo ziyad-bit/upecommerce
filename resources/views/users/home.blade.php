5d14c9785675a0154d65fd358e94beffe9273e81
ziyad-bit <ziyadali1923@yahoo.com>
2021-04-01 04:17:20 +0200

    g

--- resources/views/users/home.blade.php
+++ resources/views/users/home.blade.php
@@ -2,6 +2,25 @@
 
 @section('content')
 <div class="container">
+    @if (session('resent'))
+        <div class="alert alert-success text-center" role="alert">
+            {{ __('A fresh verification link has been sent to your email address.') }}
+        </div>
+    @endif
+
+    @if (Auth::user()->email_verified_at == null)
+        <div class="alert alert-warning text-center">
+            please verify your email
+            if you didn't recieve verification link click resend again
+            <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
+                @csrf
+                <button type="submit" style="margin-left:5px " class="btn btn-primary">{{ __('resend again') }}</button>.
+            </form> 
+        </div>
+
+        
+    @endif
+
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
